d813913bdc7839ca4f4123961cc852be11732300
USER <chidubemmba05@gmail.com>
2026-09-02 17:24:47 +0100

    added method add and deduct

--- Product.java
+++ Product.java
@@ -2,4 +2,11 @@ public class Product {
     public int product(int num1, int num2){
         return num1*num2;
     }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int deduct(int a, int b){
+        return a-b;
+    }
 }
